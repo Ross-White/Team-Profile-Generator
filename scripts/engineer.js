01645043539427851817c9e458aa1d9c8ef49ff9
Ross White <rosswhite@outlook.com>
2021-04-29 18:17:44 +0100

    FEAT: Adding questions

---
+++ scripts/engineer.js
@@ -0,0 +1,23 @@
+
+const questions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is your name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is your employee ID?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email address."
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Please enter your GitHub user name."
+    }
+];
---
+++ scripts/manager.js
@@ -0,0 +1,24 @@
+
+
+const questions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is your name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is your employee ID?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email address."
+    },
+    {
+        type: "input",
+        name: "number",
+        message: "Please enter your office number."
+    }
+];
