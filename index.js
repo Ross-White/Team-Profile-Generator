becde4c3686ddab55016a605c03ff852d2a7e2e7
Ross White <rosswhite@outlook.com>
2021-04-30 16:52:00 +0100

    HTML/CSS template

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const Employee = require('./lib/employee');
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
@@ -119,7 +118,5 @@ function addIntern() {
         createTeam()
     })
 }
-
-
 
 createManager();
