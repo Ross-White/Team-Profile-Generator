d39214366161330feb3748db7615315532fcaa4e
Ross White <rosswhite@outlook.com>
2021-05-02 20:10:02 +0100

    Adding Readme

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
+# Team Profile Generator
+
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+  ## Description
+  My task was to build a Node.js command-line application that takes in information about employees on a software engineering team, then generates an HTML webpage that displays summaries for each person.
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Contributers](#contributers)
+  * [Questions](#questions)
+  * [License](#license)
+
+  ## Installation
+  The required dependencies are installed using: nmp install
+
+  ## Contributers
+  Ross White contributed to the final outcome of this project.
+
+  ## Questions
+  For any questions regarding this project, I can be contacted through:
+  * GitHub- <github.com/ross-white>
+  * Email- <rosswhite@outlook.com>
+
+  ## License 
+  
+    This project is licensed under the MIT license.
+
+  
 # Team-Profile-Generator
+
--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 const CSS = require('./src/styles');
-const HTML = require('./src/htmlTemplate');
 
 const teamMembers = [];
 
@@ -127,8 +126,6 @@ function addIntern() {
         createTeam()
     })
 }
-
-createManager()
  
 function createFile(teamMembers) {
     if (!fs.existsSync(outputPath)) {
@@ -157,7 +154,9 @@ function renderHTML() {
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Team Generator</title>
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">    <link rel="stylesheet" href="styles.css">
+            <link rel="stylesheet" href="./styles.css">
         </head>
         <body>
             <header class="jumbotron text-center">
@@ -171,3 +170,9 @@ function renderHTML() {
         </body>
         </html>`
     };
+
+function init() {
+    createManager()
+};
+
+init();
