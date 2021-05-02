1ef90a431e1b45bd4a23c0aaac0eb8570b1faa02
Ross White <rosswhite@outlook.com>
2021-05-02 16:36:57 +0100

    FEAT: Generating HTML doc

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+// const util = require('util');
+const path = require('path');
+
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 
 const teamMembers = [];
+const outputPath = path.resolve(__dirname, "output");
+const filePath = path.join(outputPath, "index.html");
 
 function createManager() {
     inquirer.prompt([
@@ -55,6 +60,7 @@ function createTeam() {
                     break;
                 default:
                     console.log(teamMembers);
+                    createFile(teamMembers);
             }
         });            
 }
@@ -119,4 +125,41 @@ function addIntern() {
     })
 }
 
-createManager();
+createManager()
+ 
+function createFile(teamMembers) {
+    if (!fs.existsSync(outputPath)) {
+        fs.mkdirSync(outputPath);
+      }
+      fs.writeFileSync(filePath, renderHTML(teamMembers), "utf-8");
+
+}
+
+function renderHTML() {
+    const cardHTML = [];
+    cardHTML.push(teamMembers.filter(teamMember => teamMember.getRole() === 'Manager').map(manager => manager.createManagerCard()));
+    cardHTML.push(teamMembers.filter(teamMember => teamMember.getRole() === 'Engineer').map(engineer => engineer.createEngineerCard()));
+    cardHTML.push(teamMembers.filter(teamMember => teamMember.getRole() === 'Intern').map(intern => intern.createInternCard()));
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Generator</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">    <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <header class="jumbotron text-center">
+        <h1>Team Profiles</h1>
+        </header>
+        <main class="grid-container">
+            <div class="col-md-6 col-lg-4">
+            ${cardHTML.join('')}
+            </div>
+        </main>
+        
+    </body>
+    </html>`
+};
